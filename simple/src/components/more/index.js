572675d9f5b130e115dc1d08d1b7925a7b662d72
Barry de Graaff <4353213+barrydegraaff@users.noreply.github.com>
2023-08-29 09:41:45 +0200

    remove old zimlet guide

--- simple/src/components/more/index.js
+++
@@ -1,8 +0,0 @@
-import { createElement } from 'preact';
-import MoreMenu from '../more-menu';
-
-export default function createMore(context, menuItemText) {
-	return props => (
-		<MoreMenu {...props}>{{context, menuItemText}}</MoreMenu>
-	);
-}
